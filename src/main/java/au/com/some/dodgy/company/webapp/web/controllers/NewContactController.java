eaa444bc40b7a37478d35b621725915b419a5a7a
Brett Salisbury <brett.salisbury@gmail.com>
2012-05-14 21:35:08 +1000

    - Form validation now in place - redirects to add user page if model is invalid

--- src/main/java/au/com/some/dodgy/company/webapp/web/controllers/NewContactController.java
+++ src/main/java/au/com/some/dodgy/company/webapp/web/controllers/NewContactController.java
@@ -1,7 +1,10 @@
 package au.com.some.dodgy.company.webapp.web.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,7 +24,10 @@ public class NewContactController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public String onNewContact() {
+	public String onNewContact(@Valid NewContactFormModel newContact, BindingResult result) {
+		if (result.hasErrors()) {
+			return "newContact";
+		}
 		return "redirect:welcomePage";
 	}	
 }
--- src/test/java/au/com/some/dodgy/company/webapp/web/controllers/NewContactControllerTest.java
+++ src/test/java/au/com/some/dodgy/company/webapp/web/controllers/NewContactControllerTest.java
@@ -10,58 +10,72 @@ import static org.springframework.test.web.ModelAndViewAssert.assertViewName;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.mock.web.portlet.MockActionRequest;
 import org.springframework.test.web.ModelAndViewAssert;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.servlet.ModelAndView;
 
 import au.com.some.dodgy.company.webapp.web.formobjects.NewContactFormModel;
 
-
 public class NewContactControllerTest {
-	
+
 	private NewContactController newContactController;
 
 	@Before
-	public void setUp()
-	{
+	public void setUp() {
 		this.newContactController = new NewContactController();
 	}
-	
+
 	@Test
-	public void shouldReturnNewContactViewOnPageLoad()
-	{
+	public void shouldReturnNewContactViewOnPageLoad() {
 		// Given
 		String expectedViewName = "newContact";
-		
+
 		// When
-		ModelAndView actualModelAndView = this.newContactController.onPageLoad();
-		
+		ModelAndView actualModelAndView = this.newContactController
+				.onPageLoad();
+
 		// Then
 		assertViewName(actualModelAndView, expectedViewName);
 	}
-	
+
 	@Test
-	public void shouldReturnEmptyNewContractFormModelOnPageLoad()
-	{
+	public void shouldReturnEmptyNewContractFormModelOnPageLoad() {
 		// Given
 		String expectedModelAttributeName = "newContactFormModel";
 		NewContactFormModel expectedModelObject = new NewContactFormModel();
-		
+
 		// When
-		ModelAndView newContactModelAndView = this.newContactController.onPageLoad();
-		
+		ModelAndView newContactModelAndView = this.newContactController
+				.onPageLoad();
+
 		// Then
-		assertModelAttributeValue(newContactModelAndView, expectedModelAttributeName, expectedModelObject);
+		assertModelAttributeValue(newContactModelAndView,
+				expectedModelAttributeName, expectedModelObject);
+	}
+
+	@Test
+	public void shouldRedirectToWelcomePageOnValidAddContactRequest() {
+		validateViewNameGivenModelValidity("redirect:welcomePage", true);
 	}
 	
 	@Test
-	public void shouldRedirectToWelcomePageOnValidAddContactRequest()
-	{
+	public void shouldStayInNewContactPageOnInvalidAddContactRequest() {
+		validateViewNameGivenModelValidity("newContact", false);
+	}
+
+	private void validateViewNameGivenModelValidity(String expectedViewName, boolean modelValidity) {
 		// Given
-		String expectedViewName = "redirect:welcomePage";
-		
+		BindingResult mockBindingResult = Mockito.mock(BindingResult.class);
+		BDDMockito.given(mockBindingResult.hasErrors()).willReturn(!modelValidity);
+
 		// When
-		String actualViewName = this.newContactController.onNewContact();
-		
+		String actualViewName = this.newContactController.onNewContact(null,
+				mockBindingResult);
+
 		// Then
 		assertThat(actualViewName, is(equalTo(expectedViewName)));
 	}
