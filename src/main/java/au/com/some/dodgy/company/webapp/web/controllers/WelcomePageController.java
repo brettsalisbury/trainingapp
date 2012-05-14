2af9e6d3f59c0f0e79d4216f98408b50f9557cd4
Brett Salisbury <brett.salisbury@gmail.com>
2012-05-14 14:12:18 +1000

    - Refactored controller test

--- src/main/java/au/com/some/dodgy/company/webapp/web/controllers/WelcomePageController.java
+++ src/main/java/au/com/some/dodgy/company/webapp/web/controllers/WelcomePageController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Scope("request")
 public class WelcomePageController {
 
-	protected String onInitialPageLoad() {
+	public String onInitialPageLoad() {
 		return "welcomePage";
 	}
 
--- src/test/java/au/com/some/dodgy/company/webapp/web/controllers/WelcomePageControllerTest.java
+++ src/test/java/au/com/some/dodgy/company/webapp/web/controllers/WelcomePageControllerTest.java
@@ -1,22 +1,31 @@
-package au.com.some.dodgy.company.webapp.web;
+package au.com.some.dodgy.company.webapp.web.controllers;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import au.com.some.dodgy.company.webapp.web.controllers.WelcomePageController;
 
-public class WelcomePageControllerTest extends WelcomePageController {
+public class WelcomePageControllerTest {
 
+	private WelcomePageController welcomePageController;
+
+	@Before
+	public void setUp()
+	{
+		this.welcomePageController = new WelcomePageController();
+	}
+	
 	@Test
 	public void testShouldReturnHelloWorldViewOnPageLoad() {
 		// given
-		String expectedViewName = "helloWorld";
+		String expectedViewName = "welcomePage";
 		
 		// when
-		String actualViewName = this.onInitialPageLoad();
+		String actualViewName = this.welcomePageController.onInitialPageLoad();
 		
 		// then
 		assertThat(expectedViewName, is(equalTo(actualViewName)));
